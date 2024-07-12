1dda1e3968490944fddd7758402aadf58a57ba37
Joe Burke <joeburke@MacBook-Air-M1.local>
2024-07-12 11:41:37 -0700

    updated ssh key script for email arg

--- ssh.sh
+++ ssh.sh
@@ -2,9 +2,15 @@
 
 echo "Generating a new SSH key for GitHub..."
 
+# Prompt the user for input with a default value of 'core'
+read -p "Enter the email for key: " email
+
+# Set the default value to 'core' if no input is provided
+email=${email}
+
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
-ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
+ssh-keygen -t ed25519 -C "$email" -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
